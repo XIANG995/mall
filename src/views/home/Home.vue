3e8094ef35376ada5b435d298a7f8951cb7e56d0
XIANG995 <chaizx_neu@163.com>
2021-04-20 21:54:56 +0800

    添加轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,17 +4,40 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="(item, index) in banner" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
+import NavBar from 'components/common/navbar/NavBar';
+import {getHomeMultiData} from 'network/home';
+import {Swiper, SwiperItem} from 'components/common/swiper'
 
 
 export default {
   name: 'Home',
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
+  },
+  data() {
+    return {
+      banner: [],
+      recommend: []
+    }
+  },
+  created() {
+    getHomeMultiData().then(res => {
+      this.banner = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
+    })
   }
 }
 </script>
