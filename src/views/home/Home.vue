3d7686c38e708a1ae6c378871d694300586e787f
XIANG995 <chaizx_neu@163.com>
2021-04-21 17:37:05 +0800

    添加tabcontrol

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,22 +5,30 @@
     </nav-bar>
     <home-swiper :banner="banner"/>
     <home-recommend-view :recommend="recommend"/>
+    <home-feature-view/>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar';
 import HomeSwiper from './childComps/HomeSwiper';
 import HomeRecommendView from './childComps/HomeRecommendView';
+import HomeFeatureView from './childComps/HomeFeatureView';
+
+import TabControl from 'components/content/tabControl/TabControl';
+import NavBar from 'components/common/navbar/NavBar';
+
 import {getHomeMultiData} from 'network/home';
 
 
 export default {
   name: 'Home',
   components: {
-    NavBar,
     HomeSwiper,
     HomeRecommendView,
+    HomeFeatureView,
+    NavBar,
+    TabControl,
   },
   data() {
     return {
@@ -38,8 +46,23 @@ export default {
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
