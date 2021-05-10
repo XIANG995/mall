9ad8a1ea9b6c9a7d6176ceae0c76c0cbc19a41e2
XIANG995 <chaizx_neu@163.com>
2021-05-10 23:01:31 +0800

    详情页标题和请求数据

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="detail">
+    <detail-nav-bar></detail-nav-bar>
+  </div>
+</template>
+
+<script>
+  import DetailNavBar from './childComps/DetailNavBar'
+
+  import {getDetailData} from 'network/detail'
+
+ 
+  export default {
+    name: 'Detail',
+    data() {
+      return {
+        iid: null
+      }
+    },
+    components: {
+      DetailNavBar
+    },
+    created() {
+      this.iid = this.$route.params.iid;
+      getDetailData(this.iid).then(res => {
+        console.log(res);
+      })
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
